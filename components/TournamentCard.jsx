fce1e4ce21517580dab14b50f26a60d543680055
Burstlegacy <burstlegacy.business@gmail.com>
2026-06-01 23:47:05 +0100

    Update TournamentCard.jsx

--- components/TournamentCard.jsx
+++ components/TournamentCard.jsx
@@ -5,7 +5,7 @@ export default function TournamentCard({ title, game }) {
   return (
     <motion.div
       whileHover={{ scale: 1.05 }}
-      className="bg-[#161616] p-4 rounded-xl"
+      <div className="bg-[#161616] p-4">
     >
       <h3>{title}</h3>
       <p className="text-gray-400">{game}</p>
