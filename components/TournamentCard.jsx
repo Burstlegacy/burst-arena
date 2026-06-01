50404f9bc4672f50b62561904ee203116b92ed30
Burstlegacy <burstlegacy.business@gmail.com>
2026-06-02 00:36:44 +0100

    Update TournamentCard.jsx

--- components/TournamentCard.jsx
+++ components/TournamentCard.jsx
@@ -1,19 +1,12 @@
 "use client";
+
 import { motion } from "framer-motion";
 
 export default function TournamentCard({ title, game }) {
   return (
     <motion.div
       whileHover={{ scale: 1.05 }}
-      <div style={{
-  background: "#161616",
-  padding: "16px",
-  borderRadius: "10px",
-  cursor: "pointer"
-}}>
-    >
-      <h3>{title}</h3>
-      <p className="text-gray-400">{game}</p>
-    </motion.div>
-  );
-}
+      style={{
+        background: "#161616",
+        padding: "16px",
+        borderRadius: "10px",
