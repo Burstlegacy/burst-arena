71d15ed1f47fb894c967f3256bd6607a78e00f6f
Burstlegacy <burstlegacy.business@gmail.com>
2026-06-02 00:27:37 +0100

    Update title text in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
           color: "#ef4444",
           marginBottom: "10px"
         }}>
-          🔥 Burst Arena TEST 123
+          🔥 Burst Arena TEST 1234
         </h1>
 
         {/* TWITCH STREAM */}
